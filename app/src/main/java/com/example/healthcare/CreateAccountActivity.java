99e3fdc8ab4e223d901112e34ff0264d99eff35a
Unnisha Deb <unnisha705@gmail.com>
2021-07-13 22:13:36 +0530

    Spinner on department

--- app/src/main/java/com/example/healthcare/CreateAccountActivity.java
+++ app/src/main/java/com/example/healthcare/CreateAccountActivity.java
@@ -1,8 +1,5 @@
 package com.example.healthcare;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -14,6 +11,9 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -21,8 +21,6 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.HashMap;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
@@ -122,6 +120,7 @@ public class CreateAccountActivity extends AppCompatActivity {
         });
     }
 
+    //spinner set
     private void setSpinner() {
         ArrayAdapter deptSpinnerAdapter = ArrayAdapter.createFromResource(CreateAccountActivity.this, R.array.dept, android.R.layout.simple_spinner_item);
         deptSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
